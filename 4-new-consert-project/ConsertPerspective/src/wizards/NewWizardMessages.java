576d2f9a564583ce10fe3a9fad89d56a40cf4b84
Popa Emilia Ioana <emilia.ioana.popa@gmail.com>
2017-05-21 13:15:07 +0300

    #4 Completed wizard for Project creation

---
+++ 4-new-consert-project/ConsertPerspective/src/wizards/NewWizardMessages.java
@@ -0,0 +1,17 @@
+package wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+public class NewWizardMessages extends NLS {
+	private static final String BUNDLE_NAME = "wizards.messages"; //$NON-NLS-1$
+	public static String ConsertProjectWizard_2;
+	public static String ConsertProjectWizard_ConsertProjectNewWizard_Custom_Project;
+	public static String ConsertProjectWizard_ConsertProjectWizard_Create_something_custom;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, NewWizardMessages.class);
+	}
+
+	private NewWizardMessages() {
+	}
+}
