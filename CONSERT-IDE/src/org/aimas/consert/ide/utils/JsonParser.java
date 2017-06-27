61c6db47c59b3b980b77a3f0bf1b65fd4eeea468
Alexandru Sorici <alex.sorici@gmail.com>
2017-06-27 18:49:16 +0300

    #8 Huge restructuring. Make CONSERT-IDE great again!

---
+++ CONSERT-IDE/src/org/aimas/consert/ide/utils/JsonParser.java
@@ -0,0 +1,54 @@
+package org.aimas.consert.ide.utils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+
+public class JsonParser {
+	private static JsonParser instance;
+	private ObjectMapper mapper;
+
+	private JsonParser() {
+		mapper = new ObjectMapper();
+	}
+
+	public static JsonParser getInstance() {
+		if (instance == null) {
+			instance = new JsonParser();
+		}
+		return instance;
+	}
+
+	public boolean appendToFile(String projectName, Object model) {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		IFolder folder = project.getFolder("origin");
+		if (!project.exists()) {
+			System.out.println("project does not exist");
+			return false;
+		}
+
+		// Convert object to JSON string and save into file directly
+		try {
+			FileOutputStream out = new FileOutputStream(folder.getFile("consert.txt").getLocation().toFile(), true);
+			// JsonGenerator g =
+			// mapper.getJsonFactory().createJsonGenerator(out);
+			// mapper.writeValue(folder.getFile("consert.txt").getLocation().toFile(),
+			// model);
+			mapper.writeValue(out, model);
+			// Convert object to JSON string
+			// String jsonInString = mapper.writeValueAsString(model);
+			// System.out.println(jsonInString);
+			System.out.println(model);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+
+	}
+
+}
---
+++ CONSERT-IDE/src/org/aimas/consert/ide/wizards/ContextEntityWizard.java
@@ -0,0 +1,47 @@
+package org.aimas.consert.ide.wizards;
+
+import org.aimas.consert.ide.model.ContextEntityModel;
+import org.aimas.consert.ide.utils.JsonParser;
+import org.aimas.consert.ide.wizards.pages.WizardNewEntityPage;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ContextEntityWizard extends Wizard implements INewWizard {
+	private IWorkbench workbench;
+	private IStructuredSelection selection;
+	private WizardNewEntityPage _pageOne;
+
+	public ContextEntityWizard() {
+		super();
+		setWindowTitle(org.aimas.consert.ide.wizards.NewWizardMessages.ScratchEntity);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.workbench = workbench;
+		this.selection = selection;
+	}
+
+	@Override
+	public void addPages() {
+		super.addPages();
+		_pageOne = new WizardNewEntityPage(org.aimas.consert.ide.wizards.NewWizardMessages.ScratchEntity);
+		_pageOne.setDescription(
+				org.aimas.consert.ide.wizards.NewWizardMessages.ConsertProjectWizard_ConsertProjectWizard_Create_something_custom);
+		_pageOne.setTitle(org.aimas.consert.ide.wizards.NewWizardMessages.ScratchEntity);
+		addPage(_pageOne);
+	}
+
+	@Override
+	public boolean performFinish() {
+		String projectName = _pageOne.getProjectName();
+		ContextEntityModel model = new ContextEntityModel();
+		model.setName(_pageOne.getTextName());
+		model.setComment(_pageOne.getTextComment());
+		/* finish means adding in the consert.txt file the required fields */
+		return JsonParser.getInstance().appendToFile(projectName, model);
+	}
+
+}
