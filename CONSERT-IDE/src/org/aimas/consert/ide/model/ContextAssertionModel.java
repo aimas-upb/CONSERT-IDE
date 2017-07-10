22e6d6372051af2e3a77e0133c851cd2275cfa16
Alin Gabriel <alynarhip@gmail.com>
2017-07-10 03:09:32 +0300

    Improved underlying Context Model

--- CONSERT-IDE/src/org/aimas/consert/ide/model/ContextAssertionModel.java
+++ CONSERT-IDE/src/org/aimas/consert/ide/model/ContextAssertionModel.java
@@ -2,27 +2,9 @@ package org.aimas.consert.ide.model;
 
 import java.util.List;
 
-public class ContextAssertionModel {
-	private String name;
-	private String comment;
+public class ContextAssertionModel extends AbstractContextModel {
 	private int arity;
 	private List<ContextEntityModel> entities;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
 
 	public int getArity() {
 		return arity;
@@ -41,8 +23,8 @@ public class ContextAssertionModel {
 	}
 
 	public String toString() {
-		return "ContextAssertionModel [ name: " + name + ", comment: " + comment + ", arity: " + arity + ", entities: "
-				+ entities + " ] ";
+		return "{\"name\":\"" + getName() + "\",\"comment\":\"" + getComment() + "\",\"arity\":\"" + getArity()
+				+ "\",\"entities\":\"" + getEntities() + "\"}";
 	}
 
 }
