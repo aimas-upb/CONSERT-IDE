2b1113c92e0f2dca588f3390709b219496256a14
Alin Gabriel <alynarhip@gmail.com>
2017-07-19 00:54:05 +0300

    26-add Project Wide Model singleton following specs

---
+++ CONSERT-IDE/src/org/aimas/consert/ide/model/ProjectWideModel.java
@@ -0,0 +1,64 @@
+package org.aimas.consert.ide.model;
+
+import java.util.List;
+
+public class ProjectWideModel {
+	private static ProjectWideModel instance;
+	private List<ContextEntityModel> entities;
+	private List<ContextAssertionModel> assertions;
+
+	/**
+	 * here to defeat instantiation
+	 */
+	private ProjectWideModel() {
+	}
+
+	public static ProjectWideModel getInstance() {
+		if (instance == null) {
+			instance = new ProjectWideModel();
+		}
+		return instance;
+	}
+
+	public List<ContextEntityModel> getEntities() {
+		return entities;
+	}
+
+	public List<ContextAssertionModel> getAssertions() {
+		return assertions;
+	}
+
+	public ContextEntityModel getEntityByName(String name) {
+		for (ContextEntityModel cem : entities) {
+			if (cem.getName().equals(name)) {
+				return cem;
+			}
+		}
+		return null;
+	}
+
+	public ContextAssertionModel getAssertionByName(String name) {
+		for (ContextAssertionModel cam : assertions) {
+			if (cam.getName().equals(name)) {
+				return cam;
+			}
+		}
+		return null;
+	}
+
+	public boolean addAssertion(ContextAssertionModel cam) {
+		return assertions.add(cam);
+	}
+
+	public boolean removeAssertions(ContextAssertionModel cam) {
+		return assertions.remove(cam);
+	}
+
+	public boolean addEntity(ContextEntityModel cem) {
+		return entities.add(cem);
+	}
+
+	public boolean removeEntity(ContextEntityModel cem) {
+		return entities.remove(cem);
+	}
+}
